0e72e8e0bee4f6069380b2a14678321801bcb11f
stone <330079598@qq.com>
2022-01-12 00:24:57 +0800

    后台主页菜单增加、修改、删除操作

--- renren-fast-vue/src/views/modules/product/category.vue
+++ renren-fast-vue/src/views/modules/product/category.vue
@@ -1,34 +1,71 @@
 <template>
-  <el-tree
-    :data="menus"
-    :props="defaultProps"
-    :expand-on-click-node="false"
-    show-checkbox
-    node-key="catId"
-    :default-expanded-keys="expandedKey"
-  >
-    <span class="custom-tree-node" slot-scope="{ node, data }">
-      <span>{{ node.label }}</span>
-      <span>
-        <el-button
-          v-if="node.level <= 2"
-          type="text"
-          size="mini"
-          @click="() => append(data)"
-        >
-          Append
-        </el-button>
-        <el-button
-          v-if="node.childNodes.length == 0"
-          type="text"
-          size="mini"
-          @click="() => remove(node, data)"
-        >
-          Delete
-        </el-button>
+  <div>
+    <!-- 树形显示 -->
+    <el-tree
+      :data="menus"
+      :props="defaultProps"
+      :expand-on-click-node="false"
+      show-checkbox
+      node-key="catId"
+      :default-expanded-keys="expandedKey"
+    >
+      <span class="custom-tree-node" slot-scope="{ node, data }">
+        <span>{{ node.label }}</span>
+        <!-- 添加按钮 -->
+        <span>
+          <el-button
+            v-if="node.level <= 2"
+            type="text"
+            size="mini"
+            @click="() => append(data)"
+          >
+            Append
+          </el-button>
+          <!-- 删除按钮 -->
+          <el-button
+            v-if="node.childNodes.length == 0"
+            type="text"
+            size="mini"
+            @click="() => remove(node, data)"
+          >
+            Delete
+          </el-button>
+
+          <!-- 修改按钮 -->
+          <el-button type="text" size="mini" @click="edit(data)">
+            Edit
+          </el-button>
+        </span>
       </span>
-    </span>
-  </el-tree>
+    </el-tree>
+
+    <!-- 添加菜单的对话框显示 -->
+    <el-dialog
+      :title="title"
+      :visible.sync="dialogVisible"
+      width="30%"
+      :close-on-click-modal="false"
+    >
+      <el-form :model="category">
+        <el-form-item label="分类名称">
+          <el-input v-model="category.name" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="图标">
+          <el-input v-model="category.icon" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="计量单位">
+          <el-input
+            v-model="category.productUnit"
+            autocomplete="off"
+          ></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="submitData">确 定</el-button>
+      </span>
+    </el-dialog>
+  </div>
 </template>
 
 <script>
@@ -41,8 +78,21 @@ export default {
   props: {},
   data() {
     return {
+      title: "",
+      dialogType: "",
+      category: {
+        name: "",
+        parentCid: 0,
+        catLevel: 0,
+        showStatus: 1,
+        sort: 0,
+        catId: null,
+        productUnit: "",
+        icon: "",
+      },
       menus: [],
       expandedKey: [],
+      dialogVisible: false,
       defaultProps: {
         children: "children",
         label: "name",
@@ -59,10 +109,80 @@ export default {
         this.menus = data.data;
       });
     },
+    // 添加按钮,显示表单
     append(data) {
       console.log("append", data);
+      this.dialogType = "add";
+      this.title = "添加分类";
+      this.dialogVisible = true;
+      this.category = {};
+      this.category.parentCid = data.catId;
+      this.category.catLevel = data.catLevel * 1 + 1;
     },
-
+    // 修改按钮，显示表单
+    edit(data) {
+      this.dialogType = "edit";
+      this.title = "修改分类";
+      this.dialogVisible = true;
+      this.$http({
+        url: this.$http.adornUrl(`/product/category/info/${data.catId}`),
+        method: "get",
+      }).then(({ data }) => {
+        console.log("要修改的数据", data);
+        this.category.name = data.data.name;
+        this.category.catId = data.data.catId;
+        this.category.icon = data.data.icon;
+        this.category.parentCid = data.data.parentCid;
+      });
+    },
+    // 修改表单数据
+    editCategory() {
+      var { catId, name, icon, productUnit } = this.category;
+      var data = { catId, name, icon, productUnit };
+      this.$http({
+        url: this.$http.adornUrl("/product/category/update"),
+        method: "post",
+        data: this.$http.adornData(data, false),
+      }).then(({ data }) => {
+        this.$message({
+          message: "菜单修改成功",
+          typeof: "success",
+        });
+        this.dialogVisible = false;
+        // 刷新菜单
+        this.getMenus();
+        // 展开当前添加的父菜单
+        this.expandedKey = [this.category.parentCid];
+      });
+    },
+    submitData(data) {
+      if (this.dialogType == "add") {
+        this.addCategory();
+      }
+      if (this.dialogType == "edit") {
+        this.editCategory();
+      }
+    },
+    // 添加三级分类
+    addCategory() {
+      console.log("提交的三级分类数据", this.category);
+      this.$http({
+        url: this.$http.adornUrl("/product/category/save"),
+        method: "post",
+        data: this.$http.adornData(this.category, false),
+      }).then(({ data }) => {
+        this.$message({
+          message: "菜单保存成功",
+          type: "success",
+        });
+        this.dialogVisible = false;
+        // 刷新菜单
+        this.getMenus();
+        // 展开当前添加的父菜单
+        this.expandedKey = [this.category.parentCid];
+      });
+    },
+    // 删除按钮
     remove(node, data) {
       var ids = [data.catId];
       this.$confirm(`是否删除【${data.name}】?`, "提示", {
@@ -82,12 +202,12 @@ export default {
             });
             // 刷新新的菜单
             this.getMenus();
-            this.expandedKey = [node.parent.data.catId]
+            this.expandedKey = [node.parent.data.catId];
             // console.log("删除成功！！！");
           });
         })
         .catch(() => {});
-      console.log("remove", node, data);
+      //console.log("remove", node, data);
     },
   },
   // 计算属性 类似于 data 概念
