eeba18b6d5cb61a0c18cf8a9cfcce67b00d1e9e5
stone <330079598@qq.com>
2022-01-12 17:33:07 +0800

    菜单拖拽功能、增删改查完成

--- mall-product/src/main/java/com/stone/mall/product/controller/CategoryController.java
+++ mall-product/src/main/java/com/stone/mall/product/controller/CategoryController.java
@@ -1,21 +1,17 @@
 package com.stone.mall.product.controller;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-
+import com.stone.common.utils.R;
+import com.stone.mall.product.entity.CategoryEntity;
+import com.stone.mall.product.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import sun.util.resources.cldr.ee.CalendarData_ee_TG;
 
-import com.stone.mall.product.entity.CategoryEntity;
-import com.stone.mall.product.service.CategoryService;
-import com.stone.common.utils.PageUtils;
-import com.stone.common.utils.R;
+import java.util.Arrays;
+import java.util.List;
 
 
 
@@ -63,6 +59,17 @@ public class CategoryController {
     public R save(@RequestBody CategoryEntity category){
 		categoryService.save(category);
 
+        return R.ok();
+    }
+
+    /**
+     * 批量修改
+     * @param category
+     * @return
+     */
+    @RequestMapping("/update/sort")
+    public R updateSort(@RequestBody CategoryEntity[] category){
+        categoryService.updateBatchById(Arrays.asList(category));
         return R.ok();
     }
 
