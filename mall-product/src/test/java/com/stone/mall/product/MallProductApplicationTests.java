bff63951e6a48bac6ce708f99d026f9c30bbcf79
stone <330079598@qq.com>
2022-01-13 21:39:42 +0800

    完成OSS的简单测试

--- mall-product/src/test/java/com/stone/mall/product/MallProductApplicationTests.java
+++ mall-product/src/test/java/com/stone/mall/product/MallProductApplicationTests.java
@@ -1,5 +1,9 @@
 package com.stone.mall.product;
 
+
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClient;
+import com.aliyun.oss.OSSClientBuilder;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.stone.mall.product.entity.BrandEntity;
 import com.stone.mall.product.service.BrandService;
@@ -7,6 +11,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.List;
 
 @SpringBootTest
@@ -15,6 +22,8 @@ class MallProductApplicationTests {
 
     @Autowired
     BrandService brandService;
+    @Autowired
+    OSSClient ossClient;
 
     @Test
     void contextLoads() {
@@ -32,4 +41,30 @@ class MallProductApplicationTests {
         });
     }
 
+
+    @Test
+    public void Upload() throws FileNotFoundException {
+        // yourEndpoint填写Bucket所在地域对应的Endpoint。以华东1（杭州）为例，Endpoint填写为https://oss-cn-hangzhou.aliyuncs.com。
+        String endpoint = "oss-cn-beijing.aliyuncs.com";
+        // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
+        String accessKeyId = "LTAI5t6McHdFv4UQHjx7R8ZG";
+        String accessKeySecret = "b7GJznsO7kksmZzYWuqWbcGKpcYISw";
+        // 填写Bucket名称，例如examplebucket。
+//        String bucketName = "stone-mall-oss";
+
+        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+
+        InputStream inputStream = new FileInputStream("C:\\Users\\tao19\\Desktop\\iphone11.jpg");
+        ossClient.putObject("stone-mall-oss", "iphone11.jpg", inputStream);
+        ossClient.shutdown();
+        System.out.println("上传完成。。。。");
+    }
+
+    @Test
+    public void testUpload() throws FileNotFoundException {
+        InputStream inputStream = new FileInputStream("C:\\Users\\tao19\\Desktop\\iphone11.jpg");
+        ossClient.putObject("stone-mall-oss", "iphone11.jpg", inputStream);
+        ossClient.shutdown();
+        System.out.println("上传完成。。。。");
+    }
 }
