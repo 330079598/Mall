ae8f2a3670ca25ef90053c31c7e3b806afcc335c
stone <330079598@qq.com>
2022-01-19 14:39:52 +0800

    feater:完成商品品牌的分页查询功能

--- mall-product/src/main/java/com/stone/mall/product/service/impl/BrandServiceImpl.java
+++ mall-product/src/main/java/com/stone/mall/product/service/impl/BrandServiceImpl.java
@@ -1,7 +1,10 @@
 package com.stone.mall.product.service.impl;
 
+import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Service;
+
 import java.util.Map;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -18,9 +21,15 @@ public class BrandServiceImpl extends ServiceImpl<BrandDao, BrandEntity> impleme
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+        // 获取key
+        String key = (String) params.get("key");
+        QueryWrapper<BrandEntity> queryWrapper = new QueryWrapper<>();
+        if (StringUtils.isNotEmpty(key)) {
+            queryWrapper.eq("brand_id", key).or().like("name", key);
+        }
         IPage<BrandEntity> page = this.page(
                 new Query<BrandEntity>().getPage(params),
-                new QueryWrapper<BrandEntity>()
+                queryWrapper
         );
 
         return new PageUtils(page);
