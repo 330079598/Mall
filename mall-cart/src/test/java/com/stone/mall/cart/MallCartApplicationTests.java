c59db8048c00d881496509d1ac48345ceda06649
stone <330079598@qq.com>
2022-07-02 12:55:42 +0800

    购物车初始化

---
+++ mall-cart/src/test/java/com/stone/mall/cart/MallCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stone.mall.cart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallCartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
